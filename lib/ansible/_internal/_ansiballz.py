50e51bbecbb9a6f11cdc5e1be771d1421ce0a864
Doronin Stanislav <mugisbrows@gmail.com>
2025-04-28 13:08:25 +0300

    sitecustomize fix

--- lib/ansible/_internal/_ansiballz.py
+++ lib/ansible/_internal/_ansiballz.py
@@ -123,7 +123,7 @@ def _ansiballz_main(
         z = zipfile.ZipFile(modlib_path, mode='a')
 
         # py3: modlib_path will be text, py2: it's bytes.  Need bytes at the end
-        sitecustomize = 'import sys\nsys.path.insert(0,"%s")\n' % modlib_path
+        sitecustomize = 'import sys\\nsys.path.insert(0,"%s")\\n' % modlib_path
         sitecustomize = sitecustomize.encode('utf-8')
         # Use a ZipInfo to work around zipfile limitation on hosts with
         # clocks set to a pre-1980 year (for instance, Raspberry Pi)
